37f48020b17853aa9d22eec455d5a689ce1861e2
Jean Ferri <ferri@avioeste.local>
2023-12-22 14:47:57 -0300

    adicionado babel e webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Seu arquivo de entrada
+  output: {
+    filename: 'bundle.js', // O arquivo de saída
+    path: path.resolve(__dirname, 'dist'), // O diretório de saída
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Para todos os arquivos .js
+        exclude: /node_modules/, // Exceto a pasta node_modules
+        use: {
+          loader: 'babel-loader', // Use o babel-loader
+          options: {
+            presets: ['@babel/preset-env'], // Com o preset-env
+          }
+        }
+      }
+    ]
+  },
+  // Configurações para resolver os módulos do Three.js
+  resolve: {
+    alias: {
+      'three': path.resolve('../node_modules/three')
+    }
+  },
+};
